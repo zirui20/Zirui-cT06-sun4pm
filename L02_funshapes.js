725ba66bb39a0ef7bb397bb47739eeba23b0c694
zirui20 <firefishpro@gmail.com>
2025-07-20 08:41:11 +0000

    Jul 20, 2025, 8:41 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -1,6 +1,6 @@
 function setup(){
     createCanvas(600,400);
-    background(225, 225, 0);//0-255 background(r, g, b)
+    background(225, 225, 0);//0-255 background(r, g, b); r-red, g-green
 }
 
 function draw(){
