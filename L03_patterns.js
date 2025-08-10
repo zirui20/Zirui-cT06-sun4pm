8288afbf23b0d9ed909d89c13034bba774e77d71
zirui20 <firefishpro@gmail.com>
2025-08-10 08:37:43 +0000

    Aug 10, 2025, 8:37 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -1,17 +1,56 @@
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(600,600);
     background(220);
-    noLoop(); // Stops continuous drawing
+    // noLoop(); // Stops continuous drawing
   }
+
+
+let colorValue = 0;
+let spacing = 90;
+
+function draw(){
+//   // circle(75,200,100);
+//   // circle(225,200,100);
+//   // circle(375,200,100);
+//   // circle(525,200,100);
+
+//   for(let i=0; i<4; i++) {
+//     // fill(colorValue%225)
+//     // circle(spacing + i*spacing, 200, 40);
+//     if(1%2 === 0) {
+//         fill(0);
+
+//   } else {
+//     fill(255);
   
-function draw() {
-  // Recap 1: Repeating Circles
+// }
 
-  // Task 1: Colour Gradient
+// circle(spacing + i*spacing, 200, 40);
+// rect(0,100, width, 400);
+// circle(width/2, height/2, 100);
 
-  // Task 2: Colour Loop
+    let diameter = 50;
+    let numCircles = 5; 
+    let totalWidth = diameter * numCircles;
+    let startX = diameter/2; 
+    //let startX = diameter/2;
+    let startY = diameter/2; 
 
-  // Task 3: Row of Circles
 
-  // Task 4: Grid of Circles
+    for(let j =0; j<5; j++) {
+
+        let y = startY + j*diameter; 
+
+        for(let i=0; i<5; i++) {
+            fill(0, i*50, 0); 
+            circle(startX+i*diameter, y, diameter); 
+
+            //circle(startX+i*diameter, height/2, diameter); 
+            //circle(startX+i*diameter, diameter/2, diameter); 
+            
+        }
+    }
+
+
 }
+
--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,1 +1,11 @@
-// write your codes here
+function setup() {
+    createCanvas(400,400);
+    background(220);
+}
+
+function draw() {
+    for(let i = 0; 1<5; i++) {
+        fill(0, i*50, 0);
+        circle(25,25,50)
+    }
+}
