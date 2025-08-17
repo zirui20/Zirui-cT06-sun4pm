a3a3c52e50398255b057b7d07d92adcffb7527b9
zirui20 <firefishpro@gmail.com>
2025-08-17 08:16:27 +0000

    Aug 17, 2025, 8:16 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,19 +1,20 @@
-let shapeColor = 'blue';
-let circlesize = 5 ;
-function setup(){
-    createCanvas(400,400);
-    background(220);
-    noStroke();
+// let shapeColor = 'blue';
+// let circlesize = 5 ;
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     noStroke();
 
-}
+// }
 
-function mousePressed(){
-    shapeColor = color(random(225), random(225), random(225));
-    circlesize = 5;
-}
+// function mousePressed(){
+//     shapeColor = color(random(225), random(225), random(225));
+//     circlesize = 5;
+// }
 
-function mouseDragged(){
-    fill(shapeColor)
-    ellipse( mouseX, mouseY, circlesize, circlesize)
-    circlesize += 0.5;
-}
+// function mouseDragged(){
+//     fill(shapeColor)
+//     ellipse( mouseX, mouseY, circlesize, circlesize)
+//     circlesize += 0.5;
+// }
+
