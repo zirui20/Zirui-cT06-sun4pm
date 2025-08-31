ac70c7135c75b9bcbb2aee90c3d28665d679b7d2
zirui20 <firefishpro@gmail.com>
2025-08-31 08:19:40 +0000

    Aug 31, 2025, 8:19 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -18,11 +18,152 @@
 //     circlesize += 0.5;
 // }
 
-let rectSize = 50;
 
-function setup(){
-    createCanvas(400,400);
 
-    let centerX = width/2;
-    let centerY = height/2;
+
+
+// let rectSize = 50;
+
+// function setup(){
+//     createCanvas(400,400);
+// }
+
+// function draw(){
+//     background(220);
+
+
+//     let centerX = width/2;
+//     let centerY = height/2;
+
+//     let topLeftX = centerX - rectSize/2;
+//     let topLeftY = centerY - rectSize/2;
+
+
+//     rect(topLeftX, topLeftY, rectSize, rectSize);
+// }
+
+// function keyPressed(){
+//     rectSize = 100;
+// }
+
+// function keyReleased(){
+//     rectSize = 50;
+// }
+
+
+
+
+
+// function setup(){
+//     createCanvas(400,400);   
+//     background(220);
+//     noStroke();
+
+// }
+
+// function keyPressed(){
+//     let count = floor(random(5,21));
+
+//     for (let i =0; i <count; i++){
+//         let size = random(10,80);
+//         fill(random(225),random(225),random(225));
+
+
+//         let x = random(size/2, width-size/2)
+//         let y = random(size/2, width-size/2)
+
+//         ellipse(x,y,size,size);
+
+//     }
+
+// }
+
+// function keyReleased(){
+//     background(220);
+
+// }
+
+
+
+
+
+
+
+// let showCircle = false;
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     stroke(0);
+//     fill(255);
+// }
+
+// function draw(){
+//     background(220);
+
+//     if(showCircle){
+//         circle(width/2, height/2,100);
+//     }
+// }
+
+
+// function keyPressed(){
+//     if(key==="c"||  key ==="C"){
+//         showCircle = !showCircle
+//     }
+// }
+
+// function keyPressed(){
+//     console.log("key",key);
+//     console.log("key code", keyCode);
+// }
+
+
+
+
+
+// let shapeColor = 'white';
+
+// function setup(){
+//     createCanvas(200,200);
+//     noStroke();
+
+// }
+
+// function draw(){
+//     background(220);
+//     fill(shapeColor);
+//     circle(width/2, height/2,100);
+
+// }
+
+// function keyPressed(){
+
+//     if(keyCode===82){
+//         shapeColor = 'red';
+    
+//     }else if (keyCode===66){
+//         shapeColor = 'black';
+//     }else if (keyCode===71){
+//         shapeColor = 'green'
+//     }else if ()
+// }
+
+let shapeColor = 'yellow'; 
+
+function setup() {
+    createCanvas(600,600);
+    background(220);
+}
+
+function draw() {
+    if(mouseX<100) {
+        shapeColor = 'red';
+    } else if (mouseX>=100 && mouseX<200) {
+        shapeColor = 'green';
+    } else {
+        shapeColor = 'blue';
+    }
+
+    fill(shapeColor);
+    circle(width/2,height/2,100)
 }
---
+++ revision.js
@@ -0,0 +1,25 @@
+// function setup() {
+//     createCanvas(400,400);
+//     background(220);
+// }
+
+// function draw() {
+//        fill(2, 0, 0);
+//     rect(150,150,100,100);
+    
+// }
+
+
+
+ function setup() {
+    createCanvas(600,600);
+    background(220);
+}
+
+
+function draw() {
+    for (let i=0; i<5; i++){
+    ellipse(50 +i*50 , 50 + i*50, 50, 50)
+    fill(0, i*50, 0 d);
+    }
+}
