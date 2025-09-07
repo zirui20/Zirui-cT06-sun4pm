607d2238d0da6a23096953c9c9aeb03ac1336275
zirui20 <firefishpro@gmail.com>
2025-09-07 09:33:20 +0000

    Sep 7, 2025, 9:33 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -253,7 +253,7 @@ function setup() {
 function draw() {
     background(220);
 
-    x = constrain(x, 25, width - 25)
+    x = constrain(x, 25, width-25);
 
     if(keyIsDown(RIGHT_ARROW)) {
         x +=speed;
