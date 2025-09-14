6cd6e00f80c4a314f0717a0d100b376fde9353db
zirui20 <firefishpro@gmail.com>
2025-09-14 08:25:02 +0000

    Sep 14, 2025, 8:25 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -3,7 +3,7 @@ let soundEffect;
 
 function preload() {
     img = loadImage('assets/pico-a.png');
-    soundEffect = loadSound('assets/pop.mp3')
+    soundEffect = loadSound('assets/pop.mp3');
 }
 
 function setup() {
