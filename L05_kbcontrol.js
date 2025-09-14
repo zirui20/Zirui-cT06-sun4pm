d9faf779b697739069bbfbe92bac829b0b85d8f9
zirui20 <firefishpro@gmail.com>
2025-09-14 08:17:40 +0000

    Sep 14, 2025, 8:17 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -264,7 +264,11 @@ function draw() {
     }
     if(keyIsDown(LEFT_ARROW)) {
         x -=speed;
+    }if(keyIsDown(UP_ARROW)) {
+        y -=speed;
     }
-
+    if(keyIsDown(DOWN_ARROW)) {
+        y +=speed;
+    }
     circle(x,y,50);
 }
