577377f82e1bada99f77c6b9ff60ca4bff0c7026
zirui20 <firefishpro@gmail.com>
2025-09-14 09:30:51 +0000

    Sep 14, 2025, 9:30 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -80,10 +80,11 @@ function draw() {
 
     if (x-size/2 <= 0 ||x + size/2>= width) {
         speedX *= -1;
-        shapeColor = color(random(255), random(255), random(255))
+        shapeColor = color(random(255), random(255), random(255));
     }
     if (y-size/2 <= 0 ||y + size/2>= height) {
         speedY *= -1;
+        shapeColor = color(random(255), random(255), random(255));
     }
     x += speedX;
     y += speedY;
