008b99474c887ed1d80bb661d3c65e81c55a5764
zirui20 <firefishpro@gmail.com>
2025-09-14 08:50:30 +0000

    Sep 14, 2025, 8:50 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,1 +1,57 @@
-// write your codes here
+let img;
+let soundEffect;
+let music;
+let imgWidth = 100;
+let imgHeight = 100;
+let speed = 1;
+let x = 200;
+let y = 200;
+
+
+function preload() {
+    img = loadImage('assets/pico-a.png');
+    soundEffect = loadSound('assets/pop.mp3');
+    music = loadSound('assets/bossaNova.mp3');
+}
+
+function setup() {
+    createCanvas(400,400);
+    music.loop();
+}
+
+function draw() {
+    background('lightblue');
+
+    x = constrain(x, 0, width-imgWidth);
+    y = constrain(y, 0, height-imgHeight);
+
+    if(keyIsDown(RIGHT_ARROW)) {
+        x +=speed;
+    }
+    if(keyIsDown(LEFT_ARROW)) {
+        x -=speed;
+    }if(keyIsDown(UP_ARROW)) {
+        y -=speed;
+    }
+    if(keyIsDown(DOWN_ARROW)) {
+        y +=speed;
+
+    image(img, x, y, imgWidth, imgHeight);
+
+}
+
+function keyPressed() {
+    if(keyCode == 32) {
+        imgWidth = 50;
+        soundEffect.play();    
+    }
+    if(key == 's') {
+        music.stop();
+    }
+}
+
+function keyReleased() {
+    imgWidth =100;
+    soundEffect.pressed();
+}
+}
