551e722d5d097636b988a7917ea7009dd3d2eaa2
zirui20 <firefishpro@gmail.com>
2025-09-21 09:01:43 +0000

    Sep 21, 2025, 9:01 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,37 +1,36 @@
-let x = 200-20;
-let y = 200-20;
-let size = 40;
-let speedX = 5;
-let speedY = 5;
-let shapeColor = 'blue';
+// let x = 200-20;
+// let y = 200-20;
+// let size = 40;
+// let speedX = 5;
+// let speedY = 5;
+// let shapeColor = 'blue';
 
-function setup() {
-    createCanvas(600,400)
-}
+// function setup() {
+//     createCanvas(600,400)
+// }
 
-function draw() {
-    background(220);
-
-
-    if(x <= 0 || x >= width - size) {
-        speedX *= -1;
-        shapeColor = color(random(225), random(225), random(225));
-    }
-    if(y <= 0 || y >= height - size) {
-        speedY *= -1;
-        shapeColor = color(random(225), random(225), random(225));
-    }
-
-    x += speedX;
-    y += speedY;
-
-    noStroke();
-    fill(shapeColor);
-    rect(x,y,size,size);
+// function draw() {
+//     background(220);
 
 
-}
+//     if(x <= 0 || x >= width - size) {
+//         speedX *= -1;
+//         shapeColor = color(random(225), random(225), random(225));
+//     }
+//     if(y <= 0 || y >= height - size) {
+//         speedY *= -1;
+//         shapeColor = color(random(225), random(225), random(225));
+//     }
 
+//     x += speedX;
+//     y += speedY;
+
+//     noStroke();
+//     fill(shapeColor);
+//     rect(x,y,size,size);
+
+
+// }
 
 
 
