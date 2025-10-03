cea35719f357cbb1ccf112c1928786663bcdd26e
zirui20 <firefishpro@gmail.com>
2025-10-03 10:36:48 +0000

    Oct 3, 2025, 10:36 AM

--- L10_name.js
+++ L10_name.js
@@ -8,7 +8,7 @@ function setup() {
     displayText1 = "enter your name:";
 
     inputText = createInput();
-    inputText.position(230, height - 150);
+    inputText.position(220, height - 150);
     inputText.input(updateText);
 }
 
