adbbed79f0a3d826f28cf0c97e03d5f70c1f84d3
zirui20 <firefishpro@gmail.com>
2025-10-03 10:58:29 +0000

    Oct 3, 2025, 10:58 AM

--- L10_name.js
+++ L10_name.js
@@ -8,7 +8,7 @@ function setup() {
     createCanvas(600,400);
 
     displayText = "your name";
-    displayText
+    displayText = "your age";
     displayName = "enter your name:";
     displayAge = "enter your age:"
 
