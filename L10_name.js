744e8f331f263704bce5881b52fd4794bfc6cceb
zirui20 <firefishpro@gmail.com>
2025-10-03 10:19:35 +0000

    Oct 3, 2025, 10:19 AM

--- L10_name.js
+++ L10_name.js
@@ -10,6 +10,6 @@ function draw(){
     background('lightblue');
     fill("black");
     textSize(24);
-    textAllign(300,300);
+    textAlign(CENTER,300);
     text(displayText, 30/2 ,height/2);
 }
