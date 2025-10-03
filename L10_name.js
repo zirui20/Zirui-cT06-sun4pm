a9e6a60e4bc361553605d2d029fd773c8e34704a
zirui20 <firefishpro@gmail.com>
2025-10-03 10:57:21 +0000

    Oct 3, 2025, 10:57 AM

--- L10_name.js
+++ L10_name.js
@@ -16,7 +16,7 @@ function setup() {
     inputName.input(updateName);
     
     inputAge = createInput();
-    inputAge.position(220,height - 120)
+    inputAge.position(220,height - 90)
     
 }
 
