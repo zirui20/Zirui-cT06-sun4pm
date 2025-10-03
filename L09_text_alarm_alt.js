3a69c24ae44dcac7d7b012238b3e3ef3bfa2f137
zirui20 <firefishpro@gmail.com>
2025-10-03 09:58:51 +0000

    Oct 3, 2025, 9:58 AM

---
+++ L09_text_alarm_alt.js
@@ -0,0 +1,32 @@
+let csecond, cminute, chour;
+let ctime; // will hold the current hour, minute and second 
+
+let interval; // reference to the timer
+let countdown; // number to count
+
+
+function setup(){
+    createCanvas(600,400);
+
+    textSize(100);
+    textAlign(CENTER, CENTER); // align to the center
+    textFont('consolas');
+    countdown = 5; // set initial value of countdown
+
+}
+
+
+
+function draw(){
+    background('pink');
+    csecond = second(); // current second
+    cminute = minute(); // current minute
+    chour = hour(); // current hour
+    ctime = nf(chour,2) + ":" + nf(cminute,2) + ":" + nf(csecond,2);
+
+    // write the text
+    text(ctime, width/ 2, height/ 2);
+
+    // display the count down
+    text(countdown, width/2, height/2 + 100);
+}
