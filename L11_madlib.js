67719c1771a67e0a0eaab442b9a665b11c744adc
zirui20 <firefishpro@gmail.com>
2025-11-02 08:27:20 +0000

    Nov 2, 2025, 8:27 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -26,7 +26,7 @@
 /////////////////////////////////////////////////////////////////////
 
 let textBox1,textoBox2,textBox3,textBox4,textBox5;
-let arr = ["","","","","",""];
+let arr = ["enter","","","","",""];
 
 
 function setup() {
@@ -51,7 +51,7 @@ function draw() {
     background(220);
 
     for(let i=0; i<arr.length; i++) {
-        
+
             text(arr[i],50,100+15+i*50);
         }
         
