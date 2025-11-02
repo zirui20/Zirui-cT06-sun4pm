1ce5e408153d974dda49b8ebc8314a3992d43853
zirui20 <firefishpro@gmail.com>
2025-11-02 08:14:01 +0000

    Nov 2, 2025, 8:14 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -35,7 +35,7 @@ function setup() {
     textBox1.position(width/2 - textBox1.width/2,100);
 
     textBox2 = createInput();
-    textBox2.position(width/2 - textBox1.width/2,100);
+    textBox2.position(width/2 - textBox1.width/2,150);
 
     textBox1 = createInput();
     textBox1.position(width/2 - textBox1.width/2,100);
