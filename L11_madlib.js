2384583a0df55252689f3f76725aa2c631a99364
zirui20 <firefishpro@gmail.com>
2025-11-02 08:19:16 +0000

    Nov 2, 2025, 8:19 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -26,7 +26,7 @@
 /////////////////////////////////////////////////////////////////////
 
 let textBox1,textoBox2,textBox3,textBox4,textBox5;
-let arr = []
+let arr = ["",""]
 
 
 function setup() {
