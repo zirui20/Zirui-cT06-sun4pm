0dcb86e9c4888d90409a2ba305120a461539adc0
zirui20 <firefishpro@gmail.com>
2025-11-09 08:52:27 +0000

    Nov 9, 2025, 8:52 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -9,7 +9,7 @@ function setup() {
     createCanvas(600,400);
 
     hiddenWord = random(words);
-    hint = hiddenWord[0].toUpperCase() + ""
+    hint = hiddenWord[0].toUpperCase() + " "+ " ".repeat
     textBox = createInput();
     textBox.position(width/2 - textBox.width/2, 200);
     textBox.size(150, 30);
