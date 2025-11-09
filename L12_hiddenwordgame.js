7e8d8404e8ca2fae048eeefd5322995250f89cf5
zirui20 <firefishpro@gmail.com>
2025-11-09 08:30:06 +0000

    Nov 9, 2025, 8:30 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -9,7 +9,7 @@ function setup() {
     textBox.position(width/2 - textBox.width/2, 200);
 
     button = createButton('Guess');
-    button.position()
+    button.position(textBox.x + text)
 }
 function draw() {
     background(220);
